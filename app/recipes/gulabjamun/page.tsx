af8321947dd38ed8a7440d1795b52960f043fa36
Maryam Ansari <163015076+MaryamAnsari123@users.noreply.github.com>
2024-10-13 17:15:56 +0500

    Update page.tsx

--- app/recipes/gulabjamun/page.tsx
+++ app/recipes/gulabjamun/page.tsx
@@ -5,7 +5,7 @@ export default function biryani(){
         <div className="mt-7 mb-7">
             <h1 className="text-6xl text-center font-bold bg-gradient-to-b to-slate-500 from-slate-300 p-8">
                 GULAB JAMUN</h1>
-                <div style={{display:"flex"}} className="space-x-2">
+                <div className="flex space-x-2">
                     <Image 
                   
                     src="/img/g1.jpeg" // Path to your logo in the public folder
