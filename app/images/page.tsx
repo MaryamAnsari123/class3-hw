fe4711ca1ae46a593bb0434c7e9d6c3b36087de8
Maryam Ansari <163015076+MaryamAnsari123@users.noreply.github.com>
2024-10-13 17:31:35 +0500

    Update page.tsx

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -6,21 +6,21 @@ export default function images(){
             <h1 className="bg-gradient-to-l to-orange-300 from-cyan-400 text-4xl p-5 text-blue-600 font-bold text-center">Images</h1>
             <div className="flex space-x-2">
                     <Image 
-                    className="mt-8"
+                 
                     src="/img/b1.jpeg" // Path to your logo in the public folder
                     alt="pic1"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                 className="mt-8"
+                
                     src="/img/b2.jpeg" // Path to your logo in the public folder
                     alt="pic2"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                     className="mt-8"
+                  
                     src="/img/b3.jpeg" // Path to your logo in the public folder
                     alt="pic3"
                     width={300} // Adjust the size as needed
@@ -30,21 +30,21 @@ export default function images(){
 
                 <div className="flex space-x-2">
                     <Image 
-                    className="mt-8"
+               
                     src="/img/m1.jpeg" // Path to your logo in the public folder
                     alt="pic1"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                 className="mt-8"
+              
                     src="/img/m2.jpeg" // Path to your logo in the public folder
                     alt="pic2"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                     className="mt-8"
+                 
                     src="/img/m3.jpeg" // Path to your logo in the public folder
                     alt="pic3"
                     width={300} // Adjust the size as needed
@@ -54,21 +54,21 @@ export default function images(){
 
         <div className="flex space-x-2">
                     <Image 
-                    className="mt-8"
+                   
                     src="/img/g1.jpeg" // Path to your logo in the public folder
                     alt="pic1"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                 className="mt-8"
+             
                     src="/img/g2.jpeg" // Path to your logo in the public folder
                     alt="pic2"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                     className="mt-8"
+                
                     src="/img/g3.jpeg" // Path to your logo in the public folder
                     alt="pic3"
                     width={300} // Adjust the size as needed
