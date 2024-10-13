07178b3d2415d97e615000bb57021ca64bd5aab5
Maryam Ansari <163015076+MaryamAnsari123@users.noreply.github.com>
2024-10-13 17:16:57 +0500

    Update page.tsx

--- app/recipes/macroni/page.tsx
+++ app/recipes/macroni/page.tsx
@@ -5,23 +5,20 @@ export default function biryani(){
         <div className="mt-7 mb-7">
             <h1 className="text-6xl text-center font-bold bg-gradient-to-b to-slate-500 from-slate-300 p-8">
                 MECRONI</h1>
-                <div style={{display:"flex"}} className="space-x-52">
+                <div className="flex space-x-2">
                     <Image 
-                    className="mt-8 ml-20"
                     src="/img/m1.jpeg" // Path to your logo in the public folder
                     alt="pic1"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
 <Image 
-                 className="mt-8"
                     src="/img/m2.jpeg" // Path to your logo in the public folder
                     alt="pic2"
                     width={300} // Adjust the size as needed
                     height={100} // Adjust the size as needed 
 />
-<Image 
-                     className="mt-8 mr-20"
+<Image
                     src="/img/m3.jpeg" // Path to your logo in the public folder
                     alt="pic3"
                     width={300} // Adjust the size as needed
